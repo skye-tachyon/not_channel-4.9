495f064dd7b15c3a0cec04fd65d1766186c051d6
Ingo Molnar <mingo@kernel.org>
2017-02-03 21:11:09 +0100

    sched/headers: Make all include/linux/sched/*.h headers build standalone
    Make each header self-sufficient, so that it can be built successfully
    both in an allnoconfig and allyesconfig kernel.
    
    Also standardize the naming of their header guards.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Change-Id: Idd5deced64915be416805ddc211f688f96000153
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -1,5 +1,9 @@
-#ifndef _SCHED_SYSCTL_H
-#define _SCHED_SYSCTL_H
+#ifndef _LINUX_SCHED_SYSCTL_H
+#define _LINUX_SCHED_SYSCTL_H
+
+#include <linux/types.h>
+
+struct ctl_table;
 
 #ifdef CONFIG_DETECT_HUNG_TASK
 extern int	     sysctl_hung_task_check_count;
@@ -155,4 +159,4 @@ extern int sched_little_cluster_coloc_fmin_khz_handler(struct ctl_table *table,
 					int write, void __user *buffer,
 					size_t *lenp, loff_t *ppos);
 #endif
-#endif /* _SCHED_SYSCTL_H */
+#endif /* _LINUX_SCHED_SYSCTL_H */
