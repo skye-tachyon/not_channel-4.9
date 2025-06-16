0a0e1633448161635cf4b7a40f584d51ee8b7d49
skye-pa1n <sonicgames772@gmail.com>
2025-06-16 14:31:35 -0300

    not: Add not Build script
    bomb fr

---
+++ not.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+LLVM_PATH="/home/skye/bomb/clang/bin/"
+TC_PATH="/home/skye/bomb/clang/bin/"
+GCC_PATH="/usr/bin/"
+LLD_PATH="/usr/bin/"
+KERNEL_NAME="not_kernel-"
+MAKE="./makeparallel"
+BUILD_ENV="ARCH=arm64 CC=${TC_PATH}clang-21 CROSS_COMPILE=${TC_PATH}aarch64-linux-gnu-
+CROSS_COMPILE_ARM32=${TC_PATH}arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 PATH=$LLVM_PATH:$LLD_PATH:$PATH"  
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
+rm -rf /home/skye/bomb/out/arch/arm64/boot/Image
+rm -rf /home/skye/bomb/AnyKernel3/dtb
+rm -rf /home/skye/bomb/dtbo.img
+rm -rf .version
+rm -rf .local
+#make O=/home/skye/bomb/out clean
+make O=/home/skye/bomb/out $BUILD_ENV channel_defconfig
+
+echo "*****************************************"
+echo "*****************************************"
+
+make -j12 O=/home/skye/bomb/out $BUILD_ENV dtbs
+DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
+cat $DTB_OUT/*.dtb > /home/skye/bomb/AnyKernel3/channel/dtb
+
+make -j12 O=/home/skye/bomb/out $KERNEL_MAKE_ENV $BUILD_ENV Image
+IMAGE="/home/skye/bomb/out/arch/arm64/boot/Image"
+echo "**Build outputs**"
+ls /home/skye/bomb/out/arch/arm64/boot
+echo "**Build outputs**"
+cp $IMAGE /home/skye/bomb/AnyKernel3/channel/Image
+
+cd /home/skye/bomb/AnyKernel3/channel
+rm *.zip
+zip -r9 ${KERNEL_NAME}$(date +"%Y%m%d")+channel.zip .
+echo "The bomb has been planted."
+
